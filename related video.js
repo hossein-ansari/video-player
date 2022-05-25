5b162fd2e38774101bb5d83bb8ea355a99eec6da
hossein <hossein838485@gmail.com>
2022-05-25 17:36:01 +0430

    add producer duc and related video.js

---
+++ related video.js
@@ -0,0 +1,27 @@
+let videos = [
+    {
+        name : 'first',
+        srcImg : '',
+        srcVideo : ''
+        
+    },
+    {
+        name : 'second',
+        srcImg : '',
+        srcVideo : ''
+        
+    },
+    {
+        name : 'Third',
+        srcImg : '',
+        srcVideo : ''
+        
+    },
+    {
+        name : 'fourth',
+        srcImg : '',
+        srcVideo : ''
+        
+    }
+]
+console.log(videos[2].name);
